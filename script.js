16335f1b0e7dbcb32396eeabbd1ed93d151579a5
desc_inno <17777904+desc70865@users.noreply.github.com>
2020-05-14 00:08:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name		steam_link_regular
-// @namespace	https://github.com/desc70865/Steam_SChinese_Mark/
+// @namespace	https://github.com/desc70865/steam_link_regular/
 // @icon		https://keylol.com/favicon.ico
 // @version		0.5
 // @description	regular links with end /[appid]/ & print at console
 // @author		desc_inno
 // @match		https://keylol.com/*
-// @updateURL   https://raw.githubusercontent.com/desc70865/team_link_regular/master/script.js
+// @updateURL   https://raw.githubusercontent.com/desc70865/steam_link_regular/master/script.js
 // @require		https://cdn.staticfile.org/jquery/1.12.4/jquery.min.js
 // ==/UserScript==
 
